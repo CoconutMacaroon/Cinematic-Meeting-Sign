8052d5fa849802be761de8e21ab61daab49e94ce
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2020-04-28 15:04:32 -0700

    Create a build file for Windows users

---
+++ build.bat
@@ -0,0 +1,9 @@
+REM this builds the file
+REM you must have pyinstaller installed
+REM if you don't, run
+REM pip install pyinstaller
+REM or
+REM python -m pip install pyinstaller
+REM depending on your setup
+
+pyinstaller main.py
